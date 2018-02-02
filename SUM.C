6a5acd57c8e1eca1fbe7db90a138a115b60f8a85
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-02-02 16:15:25 +0530

    SUM.C
    CALCULATE SUM OF NATURAL NUMBERS UPTO N

---
+++ SUM.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,num,sum=0
+printf("Enter an integer numer\n");
+scanf("%d",&n);
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+}
+printf("sum of first %d natural numers=%d\n",number);
+return 0;
+}
