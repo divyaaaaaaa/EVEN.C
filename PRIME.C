be698db8def0ebd538c74fe07fe2d7b4b4e744ca
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-02-05 16:46:19 +0530

    PRIME.C
    A PRIME OR NOT YES OR NO

---
+++ PRIME.C
@@ -0,0 +1,12 @@
+#include<stdio.n>
+int main()
+{
+int prime,not;
+printf("Enter an value\n");
+scanf("%d",&val);
+for(i=1;<=val;i++)
+{
+not=not+i;
+}
+printf("not of first %d natural val=%d\n",value);
+}
