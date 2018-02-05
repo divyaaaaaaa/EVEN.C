1c856900d7a857cb5018ef98f205bad51dc5f4ad
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-02-05 16:33:29 +0530

    PALINDROME.C
    A NUMBER PALINDROME YES OR NO

---
+++ PALINDROME.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int palindrome;
+printf("Enter a palindrome\n");
+scanf("%d"'&palindrome);
+if(palindrome>=0)
+printf("%d is a yes palindrome\n ",palindrome);
+else
+printf("%d is a no palindrome\n ",palindrome);
+}
