3b02a7ab71e947d0e3336a69c0ee89ad7e95d120
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-02-09 15:52:43 +0530

    ARMSTRONG.C
    ARMSTRONG NUMBER PRINT YES OR NO

---
+++ ARMSTRONG.C
@@ -0,0 +1,12 @@
+#include<stdion>
+int main()
+{
+int armstrong num;
+printf("Enter a number\n");
+scanf("%d",&number);
+if(number>=0)
+printf("%d is a yes number\n",number);
+else
+printf("%d is a no number\n",number);
+getch();
+}
