929e3bc42944f4b2bf7171a6648171701f503fb9
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-02-09 16:51:53 +0530

    FIND.C
    GIVEN A NUMBER  N FIND FIRST 5 MULTIPLES  AND PRINT IT

---
+++ FIND.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int sum=0,i,n;
+clrscr();
+printf("\n please given the value of N:")
+SCANF("%d",&n);
+{
+printf("\n 5 * %d=%d,i,5*i);
+}
+getch();
+}
