385b7bb53de825f96ca05e5a8e02f8eabea0f575
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-02-19 15:59:59 +0530

    TIME.C
    FIND THE SUM OF AN ARITHMETIC PROGRAM

---
+++ TIME.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,num,sum=0;
+printf("Enter the value\n");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+}
+printf("sum of first %d natural numbers=%d\n number);
+}
