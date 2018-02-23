e66a85d9e9131cc0d13d45ae34f0be8b81c466dc
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-02-23 10:27:35 +0000

    HOUR.C
    ARRAY

---
+++ HOUR.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a[25],n,i;
+float mean=0,sum0;
+printf("Enter the numbers of terms:");
+scanf("%d",&n);
+printf("\n enter the numbers:\n");
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<=n;i++)
+{
+sum=sum+a[i];
+avg=sum\n
+}
+printf("\n mean of entered numbers are:%f",mean);
+return 0;
+}
