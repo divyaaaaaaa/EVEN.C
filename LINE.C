e2ab42aedba32c9a324e234550b2a4f9f31a3506
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-02-28 16:20:12 +0530

    LINE.C
    FIND THE VALUE

---
+++ LINE.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+char n[50];
+int i,count=1;
+gets(a);
+for(i=0;n[i]!='\0';i++)
+{
+if(n[i] == '.')
+count++;
+}
+printf("%d",count);
+return 0;
+}
