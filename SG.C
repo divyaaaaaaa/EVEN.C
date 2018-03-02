97e9e39755d5a92dd83a29b93523520cd6dc86a9
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-03-02 16:00:33 +0530

    Create SG.C

---
+++ SG.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,a[30],temp;
+{
+scanf("%d",&a[n]);
+}
+for(i=1;i<=10;i++)
+{
+if(a[n]>a[n+1])
+{
+temp=a[n];
+a[n]=a[n+1];
+a[n+1]=temp;
+}
+}
+printf("The Maximum Element is %d",a[n]);
+}
