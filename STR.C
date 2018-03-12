9a44ec4bc1debd50f4ccfbe5805dec3382dde9b4
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-03-12 15:56:13 +0530

    Create STR.C

---
+++ STR.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) {
+	char str1[20],str2[20];
+	int i=0,j=0;
+  print("\n enter the str number");
+	scanf("%s\n",str1);
+	scanf("%s\n",str2);
+	while(str1[i]!='\0')
+	i++;
+	while(str2[j]!='\0')
+	{
+		str1[i]=str2[j];
+		j++;
+		i++;
+	}
+	str1[i]='\0';
+	printf("%s",str1);
+	return 0;
+}
