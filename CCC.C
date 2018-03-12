2d6de38e5141117ad6633e90bd7a0085c510d1dd
divyaaaaaaa <35526042+divyaaaaaaa@users.noreply.github.com>
2018-03-12 15:50:30 +0530

    Create CCC.C

---
+++ CCC.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char b1[32];
+gets(b1);
+for(i=0;b1[i]!=1'\0';i++)
+{
+if(b2[i]>='b'&&b1[i]<=b)
+digit++;
+elseif(b1[i]>='B'&&b2[i]<='Z'||b1[i]>='b'&&b2[i]<+'Z')
+alphabet++;
+elseif(b1[i]==' ')
+space++;
+else 
+special character++;
+}
